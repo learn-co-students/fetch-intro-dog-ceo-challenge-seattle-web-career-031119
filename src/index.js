ce45152244ad6e576eb67da694b101e74aafff02
Sofia <jonssonsofia93@gmail.com>
2019-04-24 16:01:54 -0700

    third bullet point

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 //global dogPic array to pass into
 DOGPICS = []
 
-
 console.log('%c HI', 'color: firebrick')
 
 const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
 const dogPic= document.getElementById("dog-pic")
+const breedUrl = 'https://dog.ceo/api/breeds/list/all'
 
 function loadImage(){
 	fetch("https://dog.ceo/api/breeds/image/random/4")
@@ -22,22 +22,41 @@ function addDog(url){
 	let img= document.createElement('img')
 	img.src = url
 	dogsList.appendChild(img)
-
 }
 
-//
-// function displayDogPics(dogs){
-// 	DOGPICS = dogs
-// 	for (let i =0; i < dogs.length; i++){
-// 		let dog = dogs[i]
-		// addDog(dog)
-// 	}
-//
-// }
+function fetchBreeds(){
 
-// function addDog(dog){
-// 	let message = dog.message
-// }
-//
-// function handleSubmit
+	fetch('https://dog.ceo/api/breeds/list/all')
+	.then(response => {return response.json() })
+	.then(json => {
+		console.log('fetch response', json)
+
+	// 	for (let i =0; i < json.message.length; i++) {
+	// 	displayBreeds(json.message[i])
+	// }
+	for (let dog in json.message){
+		console.log(dog + 'hello there dog')
+		displayBreeds(dog)
+	}
+	})
+}
+function displayBreeds(breed){
+	let breedList= document.getElementById('dog-breeds')
+	let li = document.createElement('li')
+	li.textContent = breed
+
+
+	// let dogColor = document.getElementById(li)
+	//append to li bc the event has to be on every individual
+	//list item
+	li.addEventListener('click', () => {
+		li.classList.add("clicked-on")
+	})
+	breedList.appendChild(li)
+}
+
+
+
+
 loadImage()
+fetchBreeds()
